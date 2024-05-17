bab2a845f78ac79abb88366dc835203c559c1128
AVIV LEVI <60aviv60@gmail.com>
2024-05-17 13:22:46 +0300

    feat: add basic telegram handler

---
+++ base/telegram_basic_handler.py
@@ -0,0 +1,16 @@
+from core.bot_handler import BotHandler
+from telegram.ext import Application
+from telegram import Update
+
+
+class TelegramBasicHandler(BotHandler):
+
+    def __init__(self, bot_token):
+        self._bot_token = bot_token
+        self._application = None
+
+    def build(self):
+        self._application = Application.builder().token(self._bot_token).build()
+
+    def run(self):
+        self._application.run_polling(allowed_updates=Update.ALL_TYPES)
