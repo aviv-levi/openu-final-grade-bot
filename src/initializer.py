c2c27ed3e7ac837f2f8a84baeb4fbccad7587f6b
AVIV LEVI <60aviv60@gmail.com>
2024-05-17 18:47:16 +0300

    feat: initialize logger

--- src/initializer.py
+++ src/initializer.py
@@ -1,9 +1,16 @@
 import os
 from src.base.bot_handlers.openu_final_bot_handler import OpenUFinalBotHandler
 from src.base.facade import Facade
+import logging
 
 
 class Initializer:
+    def initialize_logger(self):
+        log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        logging.basicConfig(format=log_format, level=logging.INFO)
+        # set higher logging level for httpx to avoid all GET and POST requests being logged
+        logging.getLogger("httpx").setLevel(logging.WARNING)
+
     def initialize_bot_token(self):
         empty_default_token = "empty"
         bot_token = os.getenv('BOT_TOKEN', empty_default_token)
@@ -12,6 +19,7 @@ class Initializer:
         return bot_token
 
     def initialize(self) -> Facade:
+        self.initialize_logger()
         bot_token = self.initialize_bot_token()
         openu_bot = OpenUFinalBotHandler(bot_token)
         return Facade(bot_handler=openu_bot)
